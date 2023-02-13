d6b0f9b8b3b6e7e161d28f552c10514caa9ac4a6
송인범 <72247209+InbumS@users.noreply.github.com>
2023-02-14 00:44:12 +0900

    Update app.py
    Signed-off-by: 송인범 <72247209+InbumS@users.noreply.github.com>

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ cfg_model_path = "models/best.pt"
 
 cfg_enable_url_download = True
 if cfg_enable_url_download:
-    url = "https://archive.org/download/bestweight_nulljoaheae/best.pt" #Configure this if you set cfg_enable_url_download to True
+    url = "https://archive.org/download/bestweight_nulljoaheae/best.pt"
     cfg_model_path = f"models/{url.split('/')[-1:][0]}" #config model path from url name
 ## END OF CFG
 
